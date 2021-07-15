4f49b5855a938d7d630d0acc0947ba7261945137
holmes <15221038479@163.com>
2021-07-15 13:24:32 +0800

    添加吃子的逻辑

--- src/views/Chess/chess-rule.js
+++ src/views/Chess/chess-rule.js
@@ -1,19 +1,20 @@
-let x = 0,
-  y = 0,
-  intX = 0,
-  intY = 0;
+let x, y, intX, intY, camp, pass;
 const rules = {
   validateRule(chess, initChess, chessList) {
+    // 初始化数据赋值
     initValue(chess, initChess);
 
-    // 公共规则
+    // 公共规则触发
     if (notMove() || outBoundary()) return false;
 
-    if (chess.rule === "vehicle") {
-      return lineRule();
-    } else {
-      return true;
-    }
+    forEachList(chessList);
+
+    // if (chess.rule === "vehicle") {
+    //   return lineRule();
+    // } else {
+    //   return true;
+    // }
+    return pass;
   },
 };
 
@@ -22,6 +23,8 @@ const initValue = (chess, initChess) => {
   y = Math.round(chess.y);
   intX = Math.round(initChess.x);
   intY = Math.round(initChess.y);
+  pass = true;
+  camp = chess.camp;
 };
 
 // 没有动
@@ -29,6 +32,26 @@ const notMove = () => x === intX && y === intY;
 
 // 脱离边界
 const outBoundary = () => x < 0 || x > 8 || y < 0 || y > 9;
+
+// 遍历数组
+const forEachList = (chessList) => {
+  chessList.forEach((el, idx) => {
+    pointOfFall(el, idx, chessList);
+  });
+};
+
+// 落点有子
+const pointOfFall = (el, idx, chessList) => {
+  if (el.x === x && el.y === y) {
+    if (el.camp === camp) {
+      // 自己的子
+      pass = false;
+    } else {
+      // 吃子
+      chessList.splice(idx, 1);
+    }
+  }
+};
 
 // 只允许走直线
 const lineRule = () => !(x !== intX && y !== intY);
