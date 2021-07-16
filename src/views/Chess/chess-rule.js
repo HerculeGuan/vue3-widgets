86571fea55f3e9d2189281dbf0ad056e4a4009b5
holmes <15221038479@163.com>
2021-07-16 13:45:41 +0800

    优化车的逻辑代码

--- src/views/Chess/chess-rule.js
+++ src/views/Chess/chess-rule.js
@@ -1,20 +1,76 @@
-let x, y, intX, intY, camp, pass, rule;
+let x, y, intX, intY, camp, pass, rule, list;
 const rules = {
   validateRule(chess, initChess, chessList) {
     // 初始化数据赋值
-    initValue(chess, initChess);
+    initValue(chess, initChess, chessList);
 
     // 公共规则触发
     if (notMove() || outBoundary()) return false;
 
-    // 一次遍历操作
-    forEachList(chessList);
-
+    switch (rule) {
+      case "vehicle":
+        vehicleRule();
+        break;
+      case "gun":
+        console.log("gun");
+        break;
+    }
+    if (pass) pointOfFall();
     return pass;
   },
 };
 
-const initValue = (chess, initChess) => {
+// 落点有子
+const pointOfFall = () => {
+  list.forEach((el, idx) => {
+    if (el.x === x && el.y === y) {
+      if (el.camp === camp) {
+        // 自己的子
+        pass = false;
+      } else {
+        // 吃子
+        list.splice(idx, 1);
+      }
+    }
+  });
+};
+
+// 车的规则
+const vehicleRule = () => {
+  list.forEach((el, idx) => {
+    if (x === intX) {
+      let maxY = y > intY ? y : intY;
+      let minY = y > intY ? intY : y;
+      if (el.x === x && el.y < maxY && el.y > minY) pass = false;
+    } else if (y === intY) {
+      let maxX = x > intX ? x : intX;
+      let minX = x > intX ? intX : x;
+      if (el.y === y && el.x < maxX && el.x > minX) pass = false;
+    } else {
+      pass = false;
+    }
+  });
+};
+
+// 炮的规则
+// const gunRule = (el) => {
+//   if (x === intX) {
+//     let maxY = y > intY ? y : intY;
+//     let minY = y > intY ? intY : y;
+//     if (el.x === x && el.y < maxY && el.y > minY) pass = false;
+//   } else if (y === intY) {
+//     let maxX = x > intX ? x : intX;
+//     let minX = x > intX ? intX : x;
+//     if (el.y === y && el.x < maxX && el.x > minX) pass = false;
+//   } else {
+//     pass = false;
+//   }
+// };
+
+export default rules;
+
+// 初始化数据
+const initValue = (chess, initChess, chessList) => {
   x = Math.round(chess.x);
   y = Math.round(chess.y);
   intX = Math.round(initChess.x);
@@ -22,56 +78,10 @@ const initValue = (chess, initChess) => {
   pass = true;
   camp = chess.camp;
   rule = chess.rule;
+  list = chessList;
 };
 
-// 没有动
-const notMove = () => x === intX && y === intY;
-
-// 脱离边界
-const outBoundary = () => x < 0 || x > 8 || y < 0 || y > 9;
-
-// 遍历数组
-const forEachList = (chessList) => {
-  chessList.forEach((el, idx) => {
-    if (rule === "vehicle") {
-      vehicleRule(el);
-    }
-    if (pass) pointOfFall(el, idx, chessList);
-  });
-};
-
-// 落点有子
-const pointOfFall = (el, idx, chessList) => {
-  if (el.x === x && el.y === y) {
-    if (el.camp === camp) {
-      // 自己的子
-      pass = false;
-    } else {
-      // 吃子
-      chessList.splice(idx, 1);
-    }
-  }
-};
-
-// 走直线
-const isCrossLine = () => !(x !== intX && y !== intY);
-
-// 车
-const vehicleRule = (el) => {
-  if (!isCrossLine()) {
-    pass = false;
-    return;
-  }
-
-  if (x === intX) {
-    let maxY = y > intY ? y : intY;
-    let minY = y > intY ? intY : y;
-    if (el.x === x && el.y < maxY && el.y > minY) pass = false;
-  } else if (y === intY) {
-    let maxX = x > intX ? x : intX;
-    let minX = x > intX ? intX : x;
-    if (el.y === y && el.x < maxX && el.x > minX) pass = false;
-  }
-};
-
-export default rules;
+// 公共规则
+const notMove = () => x === intX && y === intY; // 没有动
+const outBoundary = () => x < 0 || x > 8 || y < 0 || y > 9; // 脱离边界
+const isCrossLine = () => !(x !== intX && y !== intY); // 走直线
